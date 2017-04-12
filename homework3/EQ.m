d787284406e261ea0c8c5c320fa182caf7ab9ba7
nag92 <nagoldfarb@wpi.edu>
2017-04-12 15:13:03 -0400

    not working

--- homework3/EQ.m
+++ homework3/EQ.m
@@ -7,8 +7,8 @@
     %phidot = ( getDxy(y(1),y(2))*[xdot;ydot]*cos(y(3)) - V*getDx(y(1),y(2)))/(getTreat(y(1),y(2))*sin(y(3))); 
     %phidot = ( -V*getDx(y(1),y(2)))/(getTreat(y(1),y(2))*sin(y(3))); 
      phidot = inv( getTreat(y(1),y(2))*( cos(y(3))+sin(y(3))   ))*...
-                 (getDxy(y(1),y(2))*[xdot;ydot]*(cos(y(3))+sin(y(3)))...
-                 -V*(getDx(y(1),y(2)) + getDy(y(1),y(2))  )  );   
+                 (getDxy(y(1),y(2))*[xdot;ydot]*(cos(y(3))-sin(y(3)))...
+                 -V*(getDx(y(1),y(2)) - getDy(y(1),y(2))  )  );   
     dydt = [xdot;ydot;phidot];
 end
 
--- homework3/problem1.m
+++ homework3/problem1.m
@@ -4,9 +4,9 @@ options = odeset('Events',@stop);
 
 %%
 
-y0 = [ -1 -1 (pi/4)]
-tspan = [0 100]
-[t,y] = ode45(@EQ,tspan,y0,options)
+y0 = [ -1 -1 pi/4]
+tspan = [0 1000000]
+[t,y] = ode45(@EQ,tspan,y0)
 plot(y(:,1),y(:,2))
  
    
