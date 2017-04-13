b73dd89fbf91ac62a34086d58a2473d5437d34fa
nag92 <nagoldfarb@wpi.edu>
2017-04-12 21:40:54 -0400

    good enough

--- homework3/stop.m
+++ homework3/stop.m
@@ -1,7 +1,7 @@
 function[value, isterminal, direction] = stop( t,y )
 %STOP Summary of this function goes here
 %   Detailed explanation goes here
-value = double( (1 - y(1))<10e-4 && (1 - y(2))<10e-4 )
+value = double( abs(1 - y(2))<.1 && abs(1 - y(1))<.1 )
 isterminal = 1;  % Halt integration 
 direction = 0;   % The zero can be approached from either direction
 
