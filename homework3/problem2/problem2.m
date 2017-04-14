d7e1118e313c91dae322b60d1eacdf9ba0630126
Nuttaworn "Ham" Sujumnong <nsujumnong@wpi.edu>
2017-04-13 21:09:31 -0400

    added tolerance and minor edits in p2

--- homework3/problem2/problem2.m
+++ homework3/problem2/problem2.m
@@ -1,33 +1,24 @@
 clear all
 clc
 options = odeset('Events',@stop2);  
-final = []
-count = 0
-t= []
-y = []
-te= []
-ye = []
-ie = []
-for ii=0:.001:0.5*pi
+final2 = []
+count2 = 0
+t2= []
+y2 = []
+te2= []
+ye2 = []
+ie2 = []
+for ii2=0:.001:0.5*pi
     
-    y0 = [ -1 -1 ii]
-    tspan = [0 100]
-    [t,y,te,ye,ie]  = ode45(@EQ,tspan,y0,options);
+    y02 = [ -1 -1 ii2]
+    tspan2 = [0 100]
+    [t2,y2,te2,ye2,ie2]  = ode45(@EQ,tspan2,y02,options);
     %plot(y(:,1),y(:,2))
-    if ~isempty(te)
-        count = count +1;
-        final(count,:) = [te,ye];
+    if ~isempty(te2)
+        count2 = count2 +1;
+        final2(count2,:) = [te2,ye2];
     end
 end   
-
-%{
-final =
-
-   34.8243    0.9000    0.9543    0.1358
-   31.4679    0.9000    0.9056    0.5662
-   28.2042    0.9000    1.0287    0.9853
-
-%}
 
 %%
 
