377556246b9bdea4deb3c13a7559bd08ab071b0e
nag92 <nagoldfarb@wpi.edu>
2017-04-15 09:18:34 -0400

    fixed the path

--- homework3/problem2/problem2.m
+++ homework3/problem2/problem2.m
@@ -8,6 +8,7 @@ y2 = []
 te2= []
 ye2 = []
 ie2 = []
+%%
 for ii2=0:.001:0.5*pi %ii == heading angle of starting position
     
     y02 = [ -1 -1 ii2]
@@ -19,6 +20,7 @@ for ii2=0:.001:0.5*pi %ii == heading angle of starting position
         final2(count2,:) = [te2,ye2,ii2];
     end
 end   
+%%
 % for final [time, x,y,psi(tf)]
 %% plot the threat field
 threat_field2 = [3.0674    3.3055    3.0505; ...
@@ -26,8 +28,8 @@ threat_field2 = [3.0674    3.3055    3.0505; ...
                  3.0811    3.6606    2.9861];
 x1 = -1:1;
 x2 = -1:1;
-
-contour(X1,X2,threat_field2)
+%%
+contour(x1,x2,threat_field2)
 hold on
 %%
 %     Final
@@ -37,8 +39,8 @@ hold on
 %     26.2831    0.8661    0.7092    0.8896    0.7160
 %     27.3359    0.8207    0.7348    0.1140    0.7220];
 final2_sub = [];
-y02_sub = [ -1 -1 0.7220];
-tspan2 = [0 100]
+y02_sub = [ -1 -1 0.7150];
+tspan2 = [0  29.5]
 [t2_sub,y2_sub,te2_sub,ye2_sub,ie2_sub]  = ode45(@EQ,tspan2,y02_sub,options);
     %plot(y(:,1),y(:,2))
 %     if ~isempty(te2_sub)
